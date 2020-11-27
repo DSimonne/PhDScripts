5ef428a64158981fd62af9a5264041787075d3cc
David Simonne <davidsimonne@gmail.com>
2020-11-27 12:07:55 +0100

    Add Scripts folder again

---
+++ Scripts/PyNX/pynx-ptypy.py
@@ -0,0 +1,26 @@
+#!C:\ProgramData\Anaconda3\python.exe
+# -*- coding: utf-8 -*-
+
+# PyNX - Python tools for Nano-structures Crystallography
+#   (c) 2016-present : ESRF-European Synchrotron Radiation Facility
+#       authors:
+#         Vincent Favre-Nicolin, favre@esrf.fr
+
+from __future__ import division
+
+import sys
+from pynx.ptycho.runner import PtychoRunnerException, helptext_generic
+from pynx.ptycho.runner.ptypy import PtychoRunnerPtyPy, PtychoRunnerScanPtyPy, helptext_beamline, params
+
+
+help_text = helptext_generic + helptext_beamline
+
+
+if __name__ == '__main__':
+    try:
+        w = PtychoRunnerPtyPy(sys.argv, params, PtychoRunnerScanPtyPy)
+        w.process_scans()
+    except PtychoRunnerException as ex:
+        print(help_text)
+        print('\n\n Caught exception: %s    \n' % (str(ex)))
+        sys.exit(1)
